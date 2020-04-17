13e880cc07e6c3c9317e84a2e2a12d5b77066274
Richard BOLLET <richard.bollet@eliberty.fr>
2020-04-17 09:48:54 +0200

    fix: add Dockerfile for Jenkins Pipeline

--- jenkins/scripts/init-dblab.sh
+++ jenkins/scripts/init-dblab.sh
@@ -2,6 +2,6 @@
 
 # Si on est dans un agent Docker, il faut installer dblab
 set -x
-sh 'dblab init --environment-id=${ENV} --url=$DBLAB_STAGE_URL --token=$DBLAB_STAGE_TOKEN --insecure'
+sh 'dblab init --environment-id=staging --url=$DBLAB_STAGE_URL --token=$DBLAB_STAGE_TOKEN --insecure'
 set +x
 
