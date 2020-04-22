131ee8ea9ae2cfa8c9ed5a96f6b26ba601e4df0c
Richard BOLLET <richard.bollet@eliberty.fr>
2020-04-22 17:01:37 +0200

    fix: update

--- jenkins/scripts/test-pg.sh
+++ jenkins/scripts/test-pg.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
+## .[] | select (.id=="pr-1234") | .db.port
 
-ASSOCIATION_ID=$1
-SNAPSHOT_ID=$2
-CLONE_ID=$3
-CHANGE_AUTHOR=$4
-BRANCH_NAME=$5
-CHANGE_TARGET=$6
-CHANGE_BRANCH=$7
-CREATE_AT=$8
-REPO_NAME=$9
+ASSOCIATION_ID="${1}"
+SNAPSHOT_ID="${2}"
+CLONE_ID="${3}"
+CHANGE_AUTHOR="${4}"
+BRANCH_NAME="${5}"
+CHANGE_TARGET="${6}"
+CHANGE_BRANCH="${7}"
+CREATE_AT="${8}"
+REPO_NAME="${9}"
+PORT="${10}"
 
 echo "ASSOCIATION_ID : $ASSOCIATION_ID"
 echo "SNAPSHOT_ID : $SNAPSHOT_ID"
@@ -19,6 +21,7 @@ echo "CHANGE_TARGET : $CHANGE_TARGET"
 echo "CHANGE_BRANCH : $CHANGE_BRANCH"
 echo "CREATE_AT : $CREATE_AT"
 echo "REPO_NAME : $REPO_NAME"
+echo "PORT : $PORT"
 
 #1|dblab_pool@14-staging|building-a-multibranch-pipeline-project_PR-5|rbeliberty|PR-5|dev|feature/add-test|2020-04-02|building-a-multibranch-pipeline-project
 
