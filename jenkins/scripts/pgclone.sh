423bee3d34bcc71d85a6f4fb7cf5c4db1916b347
Richard BOLLET <richard.bollet@eliberty.fr>
2020-04-23 08:36:09 +0200

    fix: update

--- jenkins/scripts/pgclone.sh
+++ jenkins/scripts/pgclone.sh
@@ -16,7 +16,7 @@ BRANCH_NAME=$1
 REPO_NAME=$2
 ENV_CI=$3
 
-COMMAND="SELECT * FROM associations WHERE branch_name='${BRANCH_NAME}' AND repo_name='${REPO_NAME}'"
+COMMAND="SELECT * FROM association WHERE branch_name='${BRANCH_NAME}' AND repo_name='${REPO_NAME}'"
 result=$(psql -X -AF "\t" -t -h 192.168.5.6 -c "${COMMAND}")
 echo "${result}"
 
