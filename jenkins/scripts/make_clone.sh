f6032f60abc0a1949b09a7c351811efa0805c6c6
Richard BOLLET <richard.bollet@eliberty.fr>
2020-04-23 14:26:47 +0200

    fix: update

--- jenkins/scripts/make_clone.sh
+++ jenkins/scripts/make_clone.sh
@@ -5,4 +5,4 @@ ENV_CI=$2
 echo $ENV_CI
 
 dblab config switch "${ENV_CI}"
-dblab snapshot list | jq -r '.[] | select (.id | contains("staging"))'
+dblab snapshot list | jq -r --arg CI_ENV "${ENV_CI}" '.[] | select (.id | contains($CI_ENV))'
