888c37005fefb700081467c5daeb059d29c38bdc
Richard BOLLET <richard.bollet@eliberty.fr>
2020-04-23 14:02:50 +0200

    fix: update

--- jenkins/scripts/make_clone.sh
+++ jenkins/scripts/make_clone.sh
@@ -2,6 +2,7 @@
 
 REPO_NAME=$1
 ENV_CI=$2
+echo $ENV_CI
 
 dblab config switch "${ENV_CI}"
-#dblab snapshot list | jq --raw-output '.[] | select (.id | contains(${ENV_CI}))'
+dblab snapshot list | jq -r '.[] | select (.id | contains(${ENV_CI}))'
