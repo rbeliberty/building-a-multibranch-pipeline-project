11ba132248cce146831fa452363e7510a87367d7
Richard BOLLET <richard.bollet@eliberty.fr>
2020-04-28 08:40:28 +0200

    fix: update

--- jenkins/scripts/insert-association.sh
+++ jenkins/scripts/insert-association.sh
@@ -9,5 +9,5 @@ SNAPSHOT_ID=$(dblab snapshot list | jq -r --arg CI_ENV "${ENV_CI}" '[.[] | selec
 #PORT=$(echo "${CLONE_JSON}" | jq -r --arg CLONEID "${CLONE_ID}" 'select (.id | contains($CLONEID)) | .db.port')
 
 COMMAND="INSERT INTO associations (snapshot_id, clone_id, change_author, branch_name, change_target, change_branch, created_at, repo_name, port)
-VALUES ('\"${SNAPSHOT_ID}\"', '\"${CLONE_ID}\"', '\"${CHANGE_AUTHOR}\"', '\"${BRANCH_NAME}\"', '\"${CHANGE_TARGET}\"', '\"${CHANGE_BRANCH}\"', '\"${DATE}\"', '\"${REPO_NAME}\"', \"${CLONE_PORT}\");"
+VALUES ('\"${SNAPSHOT_ID}\"', '\"${CLONE_ID}\"', '\"${CHANGE_AUTHOR}\"', '\"${BRANCH_NAME}\"', '\"${CHANGE_TARGET}\"', '\"${CHANGE_BRANCH}\"', '\"${DATE}\"', '\"${REPO_NAME}\"', ${CLONE_PORT});"
 psql -h 192.168.5.6 -c "${COMMAND}"
